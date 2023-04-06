ad34688630d6a6718868ab4db478c694efa4576b
chattie <christuaik@gmail.com>
2023-04-06 07:46:29 -0700

    complete

--- js/challenge.js
+++ js/challenge.js
@@ -1,13 +1,29 @@
 let num = 0;
+let run = true;
 
 function keepTime(){
     let counter = document.querySelector('#counter');
-    console.log(counter);
-    setTimeout(() => {
-        num+=1;
-        counter.innerText = `${num}`;
-        keepTime();}, 1000);
+    if (run === true){
+        setTimeout(() => {
+            num+=1;
+            counter.innerText = `${num}`;
+            keepTime();}, 1000);
+    } else {
+        setTimeout(() => {keepTime();}, 1000);
+    }
 }
+
+document.querySelector('#pause').addEventListener('click', e => {
+    if (run === true) {
+        run = false;
+        disableButtons();
+    } else if (run === false) {
+        run = true;
+        disableButtons();
+    } else {
+        return;
+    }
+})
 
 document.querySelector("#minus").addEventListener('click', e => {
     num -=1;
@@ -36,5 +52,28 @@ function addLike(likes){
     console.log(likes.hearts);
     likes.innerText = `${likes.id} has been liked ${likes.hearts} times`
 }
+
+function disableButtons(){
+    if (run === false){
+        document.getElementById('minus').disabled = true;
+        document.getElementById('plus').disabled = true;
+        document.getElementById('heart').disabled = true;
+        document.getElementById('submit').disabled = true;
+    }else if (run === true) {
+        document.getElementById('minus').disabled = false;
+        document.getElementById('plus').disabled = false;
+        document.getElementById('heart').disabled = false;
+        document.getElementById('submit').disabled = false;
+    } else{
+        return;
+    }
+}
+
+document.getElementById('comment-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let comment = document.createElement('p');
+    comment.innerText = document.getElementById('comment-input').value;
+    document.getElementById('list').append(comment);
+})
 
 keepTime();
